f48696ef0a190936ed987ee945143d51abbdec66
Mohammad-Alsheikh <85073682+Mohammad-Alsheikh@users.noreply.github.com>
2021-05-30 20:40:44 +0300

    Initializing Project
    Created project template

---
+++ AppointmentSystemApp/AppointmentSystemApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using AppointmentSystemApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AppointmentSystemApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ AppointmentSystemApp/AppointmentSystemApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AppointmentSystemApp.Models;
+using AppointmentSystemApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppointmentSystemApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
